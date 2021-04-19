d5359441ce7f766d18ae110780d0d07c65947657
Daniel-Tavarez <63930346+Daniel-Tavarez@users.noreply.github.com>
2021-04-18 22:06:00 -0400

    Add files via upload

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoItem3
+{
+        [ApiController]
+        [Route("[controller]")]
+        
+    public class CitiesController : ControllerBase
+        {
+            public JsonResult GetCities()
+            {
+            return new JsonResult
+             (
+                 new List<object>()
+                 {
+                     new {id = 1, Name = "New York City"},
+                     new {id = 2, Name = "Antwerp"}
+                 });
+            }
+        
+    }
+}
