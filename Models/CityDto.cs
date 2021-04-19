a79316a70d79d992e31c96ffedd5286d7522d7b5
Daniel-Tavarez <63930346+Daniel-Tavarez@users.noreply.github.com>
2021-04-18 22:06:33 -0400

    Add files via upload

---
+++ Models/CityDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TodoItem3;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoItem3.Models
+{
+    public class CityDto
+    {
+        private int id;
+        private string name;
+        private string description;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+    }
+}
---
+++ Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoItem3.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        
+        private string name;
+        private string description;
+        [Required(ErrorMessage ="You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get => name; set => name = value; }
+        [MaxLength(200)]
+        public string Description { get => description; set => description = value; }
+
+
+
+    }
+}
